d5040b1664b5f6219fb7d44ff6e8ae5384007b0f
unknown <vladtepes1048@gmail.com>
2023-03-21 10:42:26 +0200

    SQL

---
+++ MySQL/Form1.cs
@@ -0,0 +1,38 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MySQL
+{
+    public partial class Form1 : Form
+    {
+        string connstr = "server=192.168.1.235;" +
+        "port=3306;" +
+        "user=REMOTE;" +
+        "password=Aa123456@;" +
+        "database=minions;";
+        MySqlConnection connect;
+
+
+
+
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
